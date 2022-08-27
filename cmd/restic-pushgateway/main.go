bfaa2b54d69ae780997a391a85146fa661d24930
Toon Schoenmakers <nighteyes1993@gmail.com>
2022-08-27 18:29:57 +0200

    Added the ability to still see stdout of restic as well

--- cmd/restic-pushgateway/main.go
+++ cmd/restic-pushgateway/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"io"
 	"os"
 
 	"github.com/schoentoon/restic-pushgateway/pkg/stats"
@@ -16,13 +17,19 @@ func main() {
 	pushGatewayAddress := flag.String("prometheus_push_gateway", "", "Address of the push gateway")
 	job := flag.String("job", "restic", "Prometheus job label")
 	instance := flag.String("instance", "restic@"+hostname, "Prometheus instance label. Should be as descriptive as possible")
+	tee := flag.Bool("tee", false, "Print the json output from restic to stdout")
 	flag.Parse()
 
 	if *pushGatewayAddress == "" {
 		logrus.Fatal("No pushgateway address specified")
 	}
 
-	resticStats, err := stats.ParseOutput(os.Stdin)
+	var in io.Reader = os.Stdin
+	if *tee {
+		in = io.TeeReader(in, os.Stdout)
+	}
+
+	resticStats, err := stats.ParseOutput(in)
 	if err != nil {
 		logrus.Fatal(err)
 	}
